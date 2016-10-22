75383a4e00e150d7015a57a09e7e23ca7b2f3455
omarosman107 <omarosman107@gmail.com>
2016-10-22 18:39:46 -0400

    beta js with polyfil

---
+++ geolocation.js
@@ -0,0 +1,33 @@
+
+import  'https://cdnjs.cloudflare.com/ajax/libs/fetch/1.0.0/fetch.min.js';
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+
+     var finishlocation = position.coords.latitude
+     + (",") + position.coords.longitude
+     fetch(
+        ('https://maps.googleapis.com/maps/api/geocode/json?latlng=') + finishlocation + ('&token=0"')
+    ).then(function(response) {
+        // Convert to JSON
+        return response.json();
+    }).then(function(tysk) {
+
+        // Yay, `j` is a JavaScript object
+
+console.log(tysk)
+console.log(finishlocation)
+console.log("You Are at " + tysk.results[0].formatted_address)
+
+
+
+
+    });
+}
+getLocation()
