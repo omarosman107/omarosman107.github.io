3eac277d697beac3979027eab8d0d5a875316f78
omarosman107 <omarosman107@gmail.com>
2017-01-13 19:15:36 -0500

    edit

--- tvshow/js/showtime.js
+++ tvshow/js/showtime.js
@@ -63,6 +63,10 @@ document.getElementsByTagName('body')[0].innerHTML =   document.getElementsByTag
            if (finishjson.episodes[i].show.aired_episodes * 1 - finishjson.episodes[i].show.seen_episodes * 1 == finishjson.episodes[i].show.aired_episodes * 1) {
               progress = "display:none;"
            }
+           var newEpi = '<div class="new">NEW</div>';
+           if (finishjson.episodes[i].is_new == true) {
+            mathleft = newEpi
+           }
            tempel = tempel + ' <tr><th scope="row"><a href="' + "player.html?" + finishjson.episodes[i].show.name.replace("(2015)", "") + " , " + finishjson.episodes[i].name + '"><div class="showimg"><img width="100%" src="' + finishjson.episodes[i].show.images.poster["4"] + '" ><div class="w3-progress-container"><div class="w3-progressbar" style="color:#f8d637;width:' + showprog * 100 + '%;' + progress + '"></div></a></div></div></th><td><h1>' + finishjson.episodes[i].show.name + '</h1><p class="episodenum">' + "S" + pad(finishjson.episodes[i].season_number) + "E" + pad(finishjson.episodes[i].number) + '</p><h5 class="epileft">' + mathleft + '</h5><h5>' + finishjson.episodes[i].name + '</h5><a data-target="#loadvideo" href="' + "player.html?" + finishjson.episodes[i].show.name.replace("(2015)", "") + " , " + finishjson.episodes[i].name + '" class="btn btn-outline-success" alt="Play"><i class="material-icons">play_arrow</i></a><a onclick="mark(' + finishjson.episodes[i].id + ')" class="btn btn-outline-success" alt="Watch"><i class="material-icons">check</i></a></td><td></td></tr>';
         }
         div.innerHTML = tempel
