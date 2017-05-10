bd8ac3ea02668f803ec756d9e270b42883ef2794
omarosman107 <omarosman107@gmail.com>
2017-05-09 22:05:42 -0400

    edit

--- tvshow/js/media.js
+++ tvshow/js/media.js
@@ -38,8 +38,9 @@ console.log('finished')
                     }
     player.play();
 
+addEventListener('canplay', function() {
 player.currentTime(localStorage[window.location.search])
-
+});
 
 
 
