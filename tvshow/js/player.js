83683b426426512e053c33bdb3b1f591ac3b04bc
omarosman107 <omarosman107@gmail.com>
2017-05-22 22:33:08 -0400

    edit

--- tvshow/js/player.js
+++ tvshow/js/player.js
@@ -240,6 +240,7 @@ var showFetch = new XMLHttpRequest();
 
    var data = JSON.parse(this.responseText)
 console.log(data)
+document.getElementsByClassName('showImg')[0].href = "https://www.tvtime.com/en/show/" + data.query.results.json.id
 document.getElementsByClassName('showImg')[0].innerHTML = '<img width="100%" src="'+ data.query.results.json.all_images.poster._[3]   +'">'
 document.getElementById('showname').href =  "https://www.tvtime.com/en/show/" + data.query.results.json.id
 
