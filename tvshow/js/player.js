58e5b01e852d1c5454e3facd50451374e834f664
omarosman107 <omarosman107@gmail.com>
2017-05-29 14:03:34 -0400

    edit

--- tvshow/js/player.js
+++ tvshow/js/player.js
@@ -4,7 +4,7 @@
 function addJS(url) {
    var s = document.createElement('script'); // Create a script element
    s.type = "text/javascript"; // optional in html5
-   s.async = true; // asynchronous? true/false
+   s.async = false; // asynchronous? true/false
    s.src = url;
    var fs = document.getElementsByTagName('script')[0]; // Get the first script
    fs.parentNode.insertBefore(s, fs);
