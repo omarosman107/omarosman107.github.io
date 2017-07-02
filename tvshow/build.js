24cf9e8c2188f41794da17d9666016d6fdfcadce
omarosman107 <omarosman107@gmail.com>
2017-07-02 19:23:18 -0400

    edit

--- tvshow/build.js
+++ tvshow/build.js
@@ -149,8 +149,10 @@ function scrollFunction() {
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  doScrolling(0,350)
+
+//  document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
 }
 /*
 function is_touch_device() {
